75593016249e6ea42c6674446fa7f3e5c26e784c
Clinton Blackburn <clintonb@vori.com>
2023-11-27 14:00:48 -0800

    Authenticating to external collector with OIDC (#1)

--- voripos-otel-collector.sh
+++ voripos-otel-collector.sh
@@ -3,10 +3,11 @@
 # This is needed to locate the otelcol-contrib executable
 export PATH="/opt/homebrew/bin:$PATH"
 
+OIDC_CLIENT_ID=$(defaults read com.vori.VoriPOS provisioned_oidcClientID)
+OIDC_CLIENT_SECRET=$(defaults read com.vori.VoriPOS provisioned_oidcClientSecret)
+OIDC_TOKEN_URL=$(defaults read com.vori.VoriPOS provisioned_oidcTokenUrl)
 OTLP_HOSTNAME=$(defaults read com.vori.VoriPOS provisioned_otlpHostname)
 OTLP_PORT=$(defaults read com.vori.VoriPOS provisioned_otlpPort)
-OTLP_USERNAME=$(defaults read com.vori.VoriPOS provisioned_otlpUsername)
-OTLP_PASSWORD=$(defaults read com.vori.VoriPOS provisioned_otlpPassword)
 VORIPOS_ENVIRONMENT=$(defaults read com.vori.VoriPOS provisioned_environment)
 VORIPOS_BANNER_ID=$(defaults read com.vori.VoriPOS provisioned_bannerID)
 VORIPOS_BANNER_NAME=$(defaults read com.vori.VoriPOS provisioned_bannerName)
@@ -15,10 +16,11 @@ VORIPOS_STORE_NAME=$(defaults read com.vori.VoriPOS provisioned_storeName)
 VORIPOS_LANE_ID=$(defaults read com.vori.VoriPOS provisioned_laneID)
 VORIPOS_LANE_NAME=$(defaults read com.vori.VoriPOS provisioned_laneName)
 
+export OIDC_CLIENT_ID
+export OIDC_CLIENT_SECRET
+export OIDC_TOKEN_URL
 export OTLP_HOSTNAME
 export OTLP_PORT
-export OTLP_USERNAME
-export OTLP_PASSWORD
 export VORIPOS_ENVIRONMENT
 export VORIPOS_BANNER_ID
 export VORIPOS_BANNER_NAME
@@ -26,7 +28,7 @@ export VORIPOS_STORE_ID
 export VORIPOS_STORE_NAME
 export VORIPOS_LANE_ID
 export VORIPOS_LANE_NAME
-export VORIPOS_OTEL_COLLECTOR_VERSION=0.1.0
+export VORIPOS_OTEL_COLLECTOR_VERSION=0.2.0
 
 echo "Data will be exported to ${OTLP_HOSTNAME}:${OTLP_PORT}"
 otelcol-contrib --config=file:$( dirname -- "$0"; )/config.yml
